354ac6d40e70ddc83bfca4dd54a1d8556fa7ee1d
Sujeet Kumar <sujeet901626@gmail.com>
2026-08-15 12:28:50 +0530

    Update 15 - Numbers Without d as Digit.cpp

--- 2026/08 - August/15 - Numbers Without d as Digit.cpp
+++ 2026/08 - August/15 - Numbers Without d as Digit.cpp
@@ -22,4 +22,36 @@ public:
     }
 };
 
-// Optimal Approach
+// Optimal Approach - Digit DP
+class Solution {
+private:
+    int dp[11][2][2];
+    int f(int ind, int tight, int started, string& s, int d) {
+        if (ind == s.size()) {
+            return 1;
+        }
+        if (dp[ind][tight][started] != -1) {
+            return dp[ind][tight][started];
+        }
+        int ans = 0;
+        int limit = 9;
+        if (tight) {
+            limit = s[ind] - '0';
+        }
+        for (int i = 0; i <= limit; i++) {
+            int ntight = tight && (i == limit);
+            int nstarted = started || (i != 0);
+            if (nstarted && i == d) {
+                continue;
+            }
+            ans += f(ind + 1, ntight, nstarted, s, d);
+        }
+        return dp[ind][tight][started] = ans;
+    }
+public:
+    int countWithout(int n, int d) {
+        memset(dp, -1, sizeof(dp));
+        string s = to_string(n);
+        return f(0, 1, 0, s, d) - 1;
+    }
+};
